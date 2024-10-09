a9d188bd1370dc481312e29c6279adc5c6c2ce94
hulj07001 <hulj07001@E-PW05CGR7>
2024-10-09 12:25:43 +0200

    Add project files.

---
+++ Kap2upp2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+namespace kap2upp2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Hur långt hoppade Elin i meter? ");
+                string elin = Console.ReadLine();
+                double elindouble = double.Parse(elin);
+                Console.Write("Hur långt hoppade Alma i meter? ");
+                string alma = Console.ReadLine();
+                double almadouble = double.Parse(alma);
+                double skillnad = elindouble - almadouble;
+
+                Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma.");
+            }
+        }
+    }
+}
